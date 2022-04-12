d896cea9905eb29a8a3f97e2fb2da856819eff79
ilagon <lorenzyadrian@gmail.com>
2022-04-12 21:58:16 +0800

    Created notes_service

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectoryException implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class UserAlreadyExist implements Exception {}
+
+class UserDoesNotExist implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
